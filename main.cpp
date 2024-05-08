58978896ca0bf59fd928ee03ff8178eff5455fbb
JunHan <ken096124@gmail.com>
2024-05-08 16:11:11 +0900

    숫자 2개 입력받아 사칙연산 결과 출력 추가

--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
+	int Num1;
+	int Num2;
+	int Sum;
+	int Sub;
+	int Multiful;
+	int Divide;
+
 	cout << "Hellow world" << endl;
+	
+	cin >> Num1;
+	cin >> Num2;
+	Sum = Num1 + Num2;
+	Sub = Num1 - Num2;
+	Multiful = Num1 * Num2;
+	Divide = Num1 / Num2;
+
+	cout << Sum << endl;
+	cout << Sub << endl;
+	cout << Multiful << endl;
+	cout << Divide << endl;
+
+	
 	return 0;
 }
